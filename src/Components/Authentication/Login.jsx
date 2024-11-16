5bf5d89c22b6794ca7bb96fea66ebadcb5113e53
Midhunpm6 <midhunpm6060@gmail.com>
2024-11-16 23:21:44 +0530

    User Authentication finished,need to create proper alerts

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import googleimg from '../../assets/google.png'
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import googleimg from "../../assets/google.png";
+import axios from "axios";
 
 const Login = () => {
-  const navigate =useNavigate()
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlelogin = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:5001/api/auth/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Login Successfully");
+        } else {
+          alert("Login failed");
+        }
+      })
+      .catch((error) => {
+        alert("login failed");
+        console.log("Login Failed" + error);
+      });
+  };
+
   return (
     <>
-    <div className="lg:flex font-poppins">
+      <div className="lg:flex font-poppins">
         <div className="h-32 lg:w-[40vw] lg:h-[100vh] bg-violet-600 opacity-75 place-content-center">
-        <h1 className="lg:flex lg:place-content-center text-white lg:text-6xl lg:mb-32 font-Kaushan text-3xl flex place-content-center">WorkBridge</h1>
+          <h1 className="lg:flex lg:place-content-center text-white lg:text-6xl lg:mb-32 font-Kaushan text-3xl flex place-content-center">
+            WorkBridge
+          </h1>
         </div>
         <div className="lg:w-[60vw] lg:h-[100vh]  lg: flex flex-col items-center lg:pt-36 pt-4 bg-slate-100 ">
           <div className="flex flex-col place-items-center bg-white m-2 p-4 lg:p-8 rounded-md  shadow-lg">
@@ -18,21 +44,32 @@ const Login = () => {
               action=""
               className="flex flex-col place-items-center p-4 lg:pt-6 "
             >
-              
               <input
                 type="email"
                 placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300"
               />
               <input
                 type="password"
                 placeholder="Enter new password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300"
               />
-              <button className="m-2 py-2 px-7 bg-violet-500 rounded-md text-white mt-3 hover:bg-violet-600">
+              <button
+                onClick={handlelogin}
+                className="m-2 py-2 px-7 bg-violet-500 rounded-md text-white mt-3 hover:bg-violet-600"
+              >
                 Login
               </button>
-             <button onClick={()=>navigate("/signup")} className='pt-4 hover:underline '>Create new account ?</button>
+              <button
+                onClick={() => navigate("/signup")}
+                className="pt-4 hover:underline "
+              >
+                Create new account ?
+              </button>
 
               <h1>or</h1>
               <div className="flex p-2 mt-2 border-2 border-gray-200 rounded-full ">
@@ -44,7 +81,7 @@ const Login = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
