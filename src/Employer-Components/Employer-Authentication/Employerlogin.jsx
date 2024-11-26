0b6a7354c43e30f0b4a3285b70f284a728eb13c9
Midhunpm6 <midhunpm6060@gmail.com>
2024-11-26 13:27:24 +0530

    Employer validation done

--- src/Employer-Components/Employer-Authentication/Employerlogin.jsx
+++ src/Employer-Components/Employer-Authentication/Employerlogin.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { axiosAuth } from '../../Axios/Axios-instance'
+import { loginValidation } from './Validation'
 
 
 
@@ -14,18 +15,29 @@ const Employerlogin = () => {
     email:"",
     password:"",
   })
+  const [error,setError]=useState("")
+
+
 
   const handleOnchange=(e)=>{
     const changeData ={...empLoginForm,[e.target.name]: e.target.value}
     setEmpLoginForm(changeData)
   }
 
+
+
+
   const handleSbumit=async(e)=>{
     e.preventDefault()
+    if (!handleValidation(e)) return
     try {
       const response=await axiosAuth.post('/emplogin',empLoginForm)
+      
       if(response.status===200){
-         toast.success("Login successfull")
+         toast.success("Login successfull",{
+          onClose:()=>navigate('/employer')
+        
+         })
       }
     } catch (error) {
       toast.error(error.response?.data?.message || "Server Error")
@@ -33,6 +45,16 @@ const Employerlogin = () => {
 
   }
 
+
+
+  const handleValidation=(e)=>{
+    e.preventDefault()
+    const errors=loginValidation(empLoginForm)
+    setError(errors)
+    return Object.keys(errors).length === 0
+
+      
+  }
   return (
     <>
       <div className=' flex flex-col items-center pt-10 h-[100vh] bg-violet-500 font-poppins  text-white '>
@@ -51,29 +73,36 @@ const Employerlogin = () => {
         <h1 className='text-3xl lg:text-4xl font-poppins font-semibold'>
           WorkBridge<span className='text-lg'>.employer</span>
         </h1>
-        <div className='mt-20 place-items-center h-[55vh] w-[25vw] bg-violet-600 rounded-md shadow-2xl'>
+        <div className='mt-20 place-items-center h-[62vh] w-[25vw] bg-violet-600 rounded-md shadow-2xl'>
           <h1 className='text-3xl mb-10 mt-10 '>Login</h1>
-          <form action='' className='flex flex-col mt-5 items-center   text-black' onSubmit={handleSbumit}>
+          <form action='' className='flex flex-col mt-5 items-center   text-black' onSubmit={handleValidation}>
             <input
               type='email'
               name='email'
               onChange={handleOnchange}
               placeholder='Enter Your Email'
-              className='py-1 px-8 outline-none rounded-sm  focus: border-2 border-gray-200'
+              className={`py-1 px-8 outline-none rounded-sm   ${error.email ? "border-2 border-red-400": "border-2 border-gray-200"}`}
+              
             />
+            {error.email && <p className='text-violet-200'>{error.email}</p>}
             <input
               type='Password'
               name='password'
               onChange={handleOnchange}
               placeholder='Enter Your Password'
-              className='mt-4 py-1 px-8 outline-none rounded-sm  focus: border-2 border-gray-200'
+              className={`mt-6 py-1 px-8 outline-none rounded-sm   ${error.email ? "border-2 border-red-400": "border-2 border-gray-200"}`}
+
             />
+            {error.password && <p className='text-violet-200'>{error.password}</p>}
+          
+
           <h1 onClick={()=>navigate('/employersignup')} className='mt-8 mb-3 text-white cursor-pointer text-sm'>Create an Account</h1>
           <div className='mb-4'>
-          
+            
           <button
             type='submit'
             className='bg-violet-800 items-center py-2 px-7 rounded-md hover:bg-violet-900 text-white '
+            onClick={handleSbumit}
           >
             Login
           </button>
--- src/Employer-Components/Employer-Authentication/Employersignup.jsx
+++ src/Employer-Components/Employer-Authentication/Employersignup.jsx
@@ -4,6 +4,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { axiosAuth } from '../../Axios/Axios-instance'
 import { useNavigate } from 'react-router-dom'
+import { registerValidation } from './Validation'
 
 
 
@@ -17,6 +18,8 @@ const Employersignup = () => {
     password: ''
   })
 
+  const[error,setError]=useState('')
+
   const handleOnchage=(e)=>{
     const changeData={...empSignupform,[e.target.name]:e.target.value}
     setEmpLoginData(changeData)
@@ -25,8 +28,12 @@ const Employersignup = () => {
    //Connecting Axios
   const handleSubmit=async(e)=>{
     e.preventDefault()
+    
+    if (!handleValidation(e)) return
     try{
     const response=await axiosAuth.post('/empregister',empSignupform)
+      console.log(response);
+      
       toast.success('Sign Up Successfull',{
         onClose:()=>navigate('/employerlogin')
       })
@@ -37,6 +44,15 @@ const Employersignup = () => {
 
 
   }
+
+  const handleValidation=(e)=>{
+    e.preventDefault()
+    const errors=registerValidation(empSignupform)
+    setError(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  
 
 
   return (
@@ -57,48 +73,55 @@ const Employersignup = () => {
         <h1 className='text-3xl lg:text-4xl font-poppins font-semibold'>
           WorkBridge<span className='text-lg'>.employer</span>
         </h1>
-        <div className='mt-20 place-items-center h-[62vh] w-[25vw] bg-violet-600 rounded-md shadow-2xl'>
+        <div className='mt-20 place-items-center h-[70vh] w-[25vw] bg-violet-600 rounded-md shadow-2xl'>
           <h1 className='text-3xl mb-10 mt-10'>Sign Up</h1>
-          <form action='' className='flex flex-col mt-5 text-black items-center' onSubmit={handleSubmit}>
+          <form action='' className='flex flex-col mt-5 text-black items-center' onSubmit={handleValidation}>
             <input
               type='text'
               name="name"
               onChange={handleOnchage}
               placeholder='Enter Your Name '
-              className='py-1 px-8 outline-none rounded-sm  focus: border-2 border-gray-200'
+              className={`py-1 px-8 outline-none rounded-sm  ${error.name ? "border-2 border-red-400" : "border-2 border-gray-200"} ` }
             />
+              {error.name && <p className='text-violet-200'>{error.name}</p>}
+
+          
             <input
               type='email'
               name='email'
               onChange={handleOnchage}
               placeholder='Enter Your Email'
-              className='mt-6 py-1 px-8 outline-none rounded-sm  focus: border-2 border-gray-200'
+              className={`mt-6 py-1 px-8 outline-none rounded-sm  ${error.name ? "border-2 border-red-400" : "border-2 border-gray-200"} ` }
+
             />
+            {error.email && <p className='text-violet-200'>{error.email}</p>}
             <input
               type='password'
               name='password'
               onChange={handleOnchage}
               placeholder='Enter Your Password'
-              className='mt-6 py-1 px-8 outline-none rounded-sm  focus: border-2 border-gray-200'
+              className={`mt-6 py-1 px-8 outline-none rounded-sm  ${error.name ? "border-2 border-red-400" : "border-2 border-gray-200"} ` }
+
             />
+            {error.password && <p className='text-violet-200'>{error.password}</p>}
             
             <h1 onClick={()=>navigate('/employerlogin')}className='mt-5 mb-3  text-white text-sm cursor-pointer hover:text-gray-200'>Click here to login</h1>
             <div className='flex place-content-center'>
               <button
             type='submit'
             className='bg-violet-800 items-center py-2 px-7 rounded-md hover:bg-violet-900 text-white '
+            onClick={handleSubmit}
           >
             Sign Up
           </button>
             </div>
-          </form>
-
-          <h1 className='text-gray-300 mb-4 mt-4'>or continue with google</h1>
+            <h1 className='text-gray-300 mb-4 mt-4'>or continue with google</h1>
           <div className='flex p-2 mt-2 border-2 border-gray-200 rounded-full  bg-white text-black'>
             <img src={googleimg} alt='' className='w-6 mr-2' />
             <p>Sign in with google</p>
             
           </div>
+          </form>
         </div>
       </div>
     </>
