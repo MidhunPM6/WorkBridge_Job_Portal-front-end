d284690c3c5fc3b3185f0ee2ab92c34674fb5e3c
Midhunpm6 <midhunpm6060@gmail.com>
2025-01-11 12:26:15 +0530

    Made some changes at NavBar

--- src/Components/Main-Page/NavBar.jsx
+++ src/Components/Main-Page/NavBar.jsx
@@ -41,11 +41,12 @@ const NavBar = () => {
     userDetails ?
   <div className="lg:ml-40 flex flex-row">
 
-    <button onClick={()=>navigate('/profile')} className="flex flex-row mr-2  py-2 px-4 border-2 border-violet-600 hover:bg-violet-600 hover:text-white rounded-md lg:ml-32 ">
+    <button onClick={()=>navigate('/profile')} className="flex flex-row mr-2  py-2 px-4 border-2 border-violet-600 hover:bg-violet-600 hover:text-white rounded-md  lg:text-sm ">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-2">
   <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
 </svg>
       {userDetails.name}
+  
     </button>
     
     </div>
