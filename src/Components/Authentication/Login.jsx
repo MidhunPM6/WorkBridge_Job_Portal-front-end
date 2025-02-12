03a3afbc4d0a60555d5b005c103455ee38c7b91f
Midhunpm6 <midhunpm6060@gmail.com>
2025-02-12 15:03:05 +0530

    Integrated google auth in user signup

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -73,115 +73,119 @@ const Login = () => {
       const res = await axiosgGoogleAuth.post(
         '/google',
         { token: response.credential },
-        { withCredentials: true },
-        
+        { withCredentials: true }
       )
       setSavedUsername(res.data.Username)
       setUserDetails(res.data.User)
-
-      console.log('Google Auth Success:', res.data)
-      alert ("successfull")
-      
+      toast.success('Logged In', {
+        onClose: () => {
+          navigate('/')
+        },
+        autoClose: 1000
+      })
     } catch (error) {
-      console.log("error" + error)
+      console.log('error' + error)
       alert('Invalid user')
     }
   }
 
   return (
     <>
-    <GoogleOAuthProvider clientId='283509074295-2c14a2o5saenni3ri9qgjl0l61rm47or.apps.googleusercontent.com'>
-      <div className='lg:flex font-poppinn  m-10 flex justify-center  md:pt-10 h-[80vh]  '>
-        <ToastContainer
-          position='top-right'
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='dark'
-        />
-        <div className='flex flex-col justify-around items-center h-full p-7 lg:w-[35vw] bg-gradient-to-b from-violet-600 to-black shadow-2xl'>
-  <h1 className='text-white lg:text-3xl text-3xl lg:tracking-[5px]'>
-    WorkBridge
-  </h1>
-  <p className='flex flex-col items-center text-sm text-white '>
-    <span>Log in to continue your job search.</span> 
-    Find the right job faster with personalized recommendations.
-  </p>
-</div>
-        
-        <div className='lg:w-[35vw]  lg: flex flex-col justify-center items-center lg:pt-0 pt-4 bg-slate-50 '>
-          <div className='flex flex-col items-center bg-white m-2 p-4 lg:p-8 rounded-md  shadow-md'>
-            <h1 className='text-2xl lg:text-2xl font-bold  '>Login</h1>
-            <form
-              action=''
-              className='flex flex-col place-items-center p-4 lg:pt-6 '
-              onSubmit={handlelogin}
-            >
-              <div className='flex flex-col'>
+      <GoogleOAuthProvider clientId='283509074295-2c14a2o5saenni3ri9qgjl0l61rm47or.apps.googleusercontent.com'>
+        <div className='lg:flex font-poppinn  m-10 flex justify-center  md:pt-10 h-[80vh]  '>
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='dark'
+          />
+          <div className='flex flex-col justify-around items-center h-full p-7 lg:w-[35vw] bg-gradient-to-b from-violet-600 to-black shadow-2xl'>
+            <h1 className='text-white lg:text-3xl text-3xl lg:tracking-[5px]'>
+              WorkBridge
+            </h1>
+            <p className='flex flex-col items-center text-sm text-gray-200 tracking-wider  '>
+              <span>Log in to continue your job search.</span>
+              Find the right job faster with personalized recommendations.
+            </p>
+          </div>
 
-              
-              <input
-                type='email'
-                name='email'
-                placeholder='Enter your email'
-                onChange={handleOnchange}
-                className= {`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${error.email ? `border-red-600`:""}`}
-              />
-              {error.email && <p className='text-red-600 ml-2 text-sm'>{error.email}</p>}
-
-              </div>
-              <div>
-              <input
-                type='password'
-                name='password'
-                placeholder='Enter new password'
-                onChange={handleOnchange}
-                className= {`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${error.password ? `border-red-600`:""}`}
-              />
-              {error.password && <p className='text-red-600 ml-2 text-sm'>{error.password}</p>}
-
-              </div>
-              
-              <button
-                type='submit'
-                className='m-2 py-1 px-5 bg-violet-600 rounded-md text-white mt-6 hover:bg-violet-700 shadow'
+          <div className='lg:w-[35vw]  lg: flex flex-col justify-center items-center lg:pt-0 pt-4 bg-slate-50 '>
+            <div className='flex flex-col items-center bg-white m-2 p-4 lg:p-8 rounded-md  shadow-md'>
+              <h1 className='text-2xl lg:text-2xl font-bold  '>Login</h1>
+              <form
+                action=''
+                className='flex flex-col place-items-center p-4 lg:pt-6 '
+                onSubmit={handlelogin}
               >
-                Login
-              </button>
-              <button
-                onClick={() => navigate('/signup')}
-                className='pt-4 hover:underline hover:underline-offset-2  text-gray-600 hover:text-black text-sm '
-              >
-                
-                Create new account ?
-              </button>
-               <div className='flex  items-center gap-2  '>
-               <hr class="h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700"></hr>
-               <h1 className='text-gray-500'>or</h1>
-               <hr class="h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700"></hr>
-               </div>
-              
-              <div className='mt-4'>
-                
+                <div className='flex flex-col'>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Enter your email'
+                    onChange={handleOnchange}
+                    className={`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${
+                      error.email ? `border-red-600` : ''
+                    }`}
+                  />
+                  {error.email && (
+                    <p className='text-red-600 ml-2 text-sm'>{error.email}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Enter new password'
+                    onChange={handleOnchange}
+                    className={`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${
+                      error.password ? `border-red-600` : ''
+                    }`}
+                  />
+                  {error.password && (
+                    <p className='text-red-600 ml-2 text-sm'>
+                      {error.password}
+                    </p>
+                  )}
+                </div>
+
+                <button
+                  type='submit'
+                  className='m-2 py-1 px-5 bg-violet-600 rounded-md text-white mt-6 hover:bg-violet-700 shadow'
+                >
+                  Login
+                </button>
+                <button
+                  onClick={() => navigate('/signup')}
+                  className='pt-4 hover:underline hover:underline-offset-2  text-gray-600 hover:text-black text-sm '
+                >
+                  Create new account ?
+                </button>
+                <div className='flex  items-center gap-2  '>
+                  <hr class='h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700'></hr>
+                  <h1 className='text-gray-500'>or</h1>
+                  <hr class='h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700'></hr>
+                </div>
+
+                <div className='mt-4'>
                   <GoogleLogin
-                    ux_mode="popup"
-                    usefqdn ={true}
+                    ux_mode='popup'
+                    usefqdn={true}
                     onSuccess={handleGoogleAuth}
                     onError={() => {
                       console.log('Login Failed')
                     }}
-                  /> 
-                
-              </div>
-            </form>
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
       </GoogleOAuthProvider>
     </>
   )
--- src/Components/Authentication/SignUp.jsx
+++ src/Components/Authentication/SignUp.jsx
@@ -4,21 +4,24 @@ import { useNavigate } from 'react-router-dom'
 import { registerValidation } from './Validation'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { axiosAuth } from '../../Axios/Axios-instance'
+import { axiosAuth, axiosgGoogleAuth } from '../../Axios/Axios-instance'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import { ContextSeekerName } from '../../Context/SeekerContext'
+import { UserContext } from '../../Context/UserDetailsContext'
 
 
-
-  const SignUp = () => {
+const SignUp = () => {
   const navigate = useNavigate()
   const [error, setError] = useState({})
-
+  const { setSavedUsername } = useContext(ContextSeekerName)
+  const { setUserDetails } = useContext(UserContext)
 
   // Craeted State for form data to store
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    mobile :'',
+    mobile: ''
   })
 
   // Onchange event
@@ -41,104 +44,161 @@ import { axiosAuth } from '../../Axios/Axios-instance'
 
     if (!handleValidation(e)) return
     try {
-       const response = await axiosAuth.post('/register', formData)
-       console.log(response);
-       
-       
-        toast.success('Login Successful', {
-            onClose: () => {
-                navigate('/login');
-            },
-            autoClose: 1000,
-        });
+      const response = await axiosAuth.post('/register', formData)
+      console.log(response)
+
+      toast.success('Login Successful', {
+        onClose: () => {
+          navigate('/login')
+        },
+        autoClose: 1000
+      })
     } catch (error) {
       toast.error(error.response?.data?.message || 'Server Error')
+    }
+  }
+  
+  // Google authentication
+  const handleGoogleAuth = async response => {
+    try {
+      const res = await axiosgGoogleAuth.post(
+        '/google',
+        { token: response.credential },
+        { withCredentials: true }
+      )
+      setSavedUsername(res.data.Username)
+      setUserDetails(res.data.User)
+
+      toast.success('Logged In',{
+        onClose: () => {
+          navigate('/')
+        },
+        autoClose: 1000
+      })
+      
+      
+    } catch (error) {
+      console.log('error' + error)
+      alert('Invalid user')
     }
   }
 
   return (
     <>
-      <div className='lg:flex font-poppins'>
-        <ToastContainer
-          position='top-right'
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='dark'
-        />
-        <div className='h-32 lg:w-[40vw] lg:h-[100vh] bg-violet-600 opacity-75 place-content-center lg'>
-          <h1 className='lg:flex lg:place-content-center text-white lg:text-6xl lg:mb-32 font-Kaushan text-3xl flex place-content-center'>
-            WorkBridge
-          </h1>
-         
-        </div>
-        <div className='lg:w-[60vw] lg:h-[100vh]  lg: flex flex-col items-center lg:pt-36 pt-4 bg-slate-100 '>
-          <div className='flex flex-col place-items-center bg-white m-2 p-4 lg:p-8 rounded-md  shadow-lg'>
-            <h1 className='text-2xl lg:text-3xl font-semibold '>Sign Up</h1>
-            <form
-              action='submit'
-              className='flex flex-col place-items-center p-4 lg:pt-6 '
-              onSubmit={handleValidation}
-            >
-              <input
-                type='text'
-                placeholder='Enter your name'
-                name='name'
-                onChange={handleChange}
-                className='m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300  '
-              />
-              {error.name && <p className='text-red-800'>{error.name}</p>}
-              <input
-                type='email'
-                placeholder='Enter your email'
-                name='email'
-                onChange={handleChange}
-                className='m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300'
-              />
-              
+      <GoogleOAuthProvider clientId='283509074295-2c14a2o5saenni3ri9qgjl0l61rm47or.apps.googleusercontent.com'>
+        <div className='lg:flex font-poppinn  m-10 flex justify-center  md:pt-10 h-[80vh]  '>
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='dark'
+          />
+          <div className='flex flex-col justify-around items-center h-full p-7 lg:w-[35vw] bg-gradient-to-b from-violet-600 to-black shadow-2xl'>
+            <h1 className='text-white lg:text-3xl text-3xl lg:tracking-[5px]'>
+              WorkBridge
+            </h1>
+            <p className='flex flex-col items-center text-sm  text-gray-200 tracking-wider '>
+              <span className=''>Join WorkBridge Today!</span>
+              Create an account to discover new job opportunities and grow your
+              career.
+            </p>
+          </div>
 
-              {error.email && <p className='text-red-800'>{error.email}</p>}
-              <input
-                type='number'
-                placeholder='Enter your number'
-                name='mobile'
-                onChange={handleChange}
-                className='m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300'
-              />
-              <input
-                type='password'
-                placeholder='Enter new password'
-                name='password'
-                onChange={handleChange}
-                className='m-2 py-2 lg:m-2 lg:py-2 lg:px-11 rounded-md flex text-start outline-none border-2  border-gray-200 hover:border-violet-200 focus:border-violet-300'
-              />
-              {error.password && (
-                <p className='text-red-800'>{error.password}</p>
-              )}
-              <button
-                type='submit'
-                className='m-2 py-2 px-7 bg-violet-500 rounded-md text-white mt-3 hover:bg-violet-600'
-                onClick={handleSubmit}
+          <div className='lg:w-[35vw]  lg: flex flex-col justify-center items-center lg:pt-0 pt-4 bg-slate-50 '>
+            <div className='flex flex-col items-center bg-white m-2 p-4 lg:p-8 rounded-md  shadow-md'>
+              <h1 className='text-2xl lg:text-2xl font-bold  '>Sign In </h1>
+              <form
+                action=''
+                className='flex flex-col place-items-center p-4 lg:pt-6 '
+                onSubmit={handleSubmit}
               >
-                Sign Up
-              </button>
-              <button
-                onClick={() => navigate('/login')}
-                className='pt-4 lg:hover:underline '
-              >
-                Already Account ?
-              </button>
+                <div className='flex flex-col'>
+                  <input
+                    type='text'
+                    name='name'
+                    placeholder='Enter your name'
+                    onChange={handleChange}
+                    className={`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${
+                      error.email ? `border-red-600` : ''
+                    }`}
+                  />
+                  {error.name && (
+                    <p className='text-red-600 ml-2 text-sm'>{error.name}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Enter your email'
+                    onChange={handleChange}
+                    className={`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${
+                      error.password ? `border-red-600` : ''
+                    }`}
+                  />
+                  {error.email && (
+                    <p className='text-red-600 ml-2 text-sm'>
+                      {error.email}
+                    </p>
+                  )}
+                </div>
 
-          
-            </form>
+                <div>
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Enter a new password'
+                    onChange={handleChange}
+                    className={`  m-2 py-1 px-8 rounded flex text-start outline-none border focus:border-gray-500 focus:border  hover:border-gray-30 bg-gray-50  ${
+                      error.password ? `border-red-600` : ''
+                    }`}
+                  />
+                  {error.password && (
+                    <p className='text-red-600 ml-2 text-sm'>
+                      {error.password}
+                    </p>
+                  )}
+                </div>
+
+                <button
+                  type='submit'
+                  className='m-2 py-1 px-5 bg-violet-600 rounded-md text-white mt-6 hover:bg-violet-700 shadow'
+                >
+                  SignUp
+                </button>
+                <button
+                  onClick={() => navigate('/login')}
+                  className='pt-4 hover:underline hover:underline-offset-2  text-gray-600 hover:text-black text-sm '
+                >
+                  Already have account ?
+                </button>
+                <div className='flex  items-center gap-2  '>
+                  <hr class='h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700'></hr>
+                  <h1 className='text-gray-500'>or</h1>
+                  <hr class='h-px my-2 w-20  bg-gray-200 border-0 dark:bg-gray-700'></hr>
+                </div>
+
+                <div className='mt-4'>
+                  <GoogleLogin
+                    ux_mode='popup'
+                    usefqdn={true}
+                    onSuccess={handleGoogleAuth}
+                    onError={() => {
+                      console.log('Login Failed')
+                    }}
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </GoogleOAuthProvider>
     </>
   )
 }
