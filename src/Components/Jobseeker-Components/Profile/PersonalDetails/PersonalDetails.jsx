a911d358f0bb142a81ba821685595bcb4b867534
Midhunpm6 <midhunpm6060@gmail.com>
2025-04-09 14:16:13 +0530

    feat: Implemented Modal before cover picture upload to preview the picture, After toast the status

--- src/Components/Jobseeker-Components/Profile/PersonalDetails/PersonalDetails.jsx
+++ src/Components/Jobseeker-Components/Profile/PersonalDetails/PersonalDetails.jsx
@@ -19,6 +19,7 @@ const PersonalDetails = () => {
   const [resume, setResume] = useState('')
   const [previewModalOpen, setPreviewModalOpen] = useState(false)
   const [selectedPreviewFile, setSelectedPreviewFile] = useState(null)
+  const [previewCoverModalOpen, setPreviewCoverModalOpen] = useState(false)
   const [previewURL, setPreviewURL] = useState('')
   const [loading, setLoading] = useState(false)
   const dispatch = useDispatch()
@@ -27,27 +28,33 @@ const PersonalDetails = () => {
 
   const navigate = useNavigate()
 
-  const handleFileSelect = e => {
+  const handleFileSelect = (e, type) => {
     const file = e.target.files[0]
-    setProfilePic(file)
+
     console.log(profilePic)
 
-    if (file) {
-      const fileType = file.type
+    const fileType = file.type
 
-      if (fileType === 'application/pdf') {
-        // Ignore PDFs
-        return
-      }
-
-      const reader = new FileReader()
-      reader.onloadend = () => {
-        setPreviewURL(reader.result)
-        setSelectedPreviewFile(file)
-        setPreviewModalOpen(true) // Open preview modal
-      }
-      reader.readAsDataURL(file)
+    if (fileType === 'application/pdf') {
+      return
     }
+
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setPreviewURL(reader.result)
+      setSelectedPreviewFile(file)
+
+      if (type === 'profilepic') {
+        setProfilePic(file)
+        setPreviewModalOpen(true)
+      }
+
+      if (type === 'coverpic') {
+        setProfileCover(file)
+        setPreviewCoverModalOpen(true)
+      }
+    }
+    reader.readAsDataURL(file)
   }
 
   //  Modal function to controll
@@ -63,7 +70,6 @@ const PersonalDetails = () => {
   }
 
   const handleProfilePicUpload = async () => {
-    console.log(profilePic)
     setLoading(true)
     setTimeout(() => setLoading(false), 10000)
 
@@ -100,8 +106,8 @@ const PersonalDetails = () => {
     }
   }
   const handleProfileCoverUpload = async () => {
-    console.log(profileCover)
-
+    setLoading(true)
+    setTimeout(() => setLoading(false), 10000)
     //  Creating formData for the file upload
     const formData = new FormData()
     formData.append('file', profileCover)
@@ -117,8 +123,20 @@ const PersonalDetails = () => {
           withCredentials: true
         }
       )
+      toast.success('Successfully Uploaded', {
+        duration: 2000
+      })
+
+      setTimeout(() => {
+        window.location.reload()
+      }, 2000)
     } catch (error) {
-      console.error(error)
+      toast.error(error.response.data.message, {
+        duration: 2000
+      })
+      setTimeout(() => {
+        navigate('/')
+      }, 2000)
     }
   }
 
@@ -182,7 +200,10 @@ const PersonalDetails = () => {
             }
           >
             <div className='absolute top-0  bottom-0 m-5  w-full flex justify-between items-end   '>
-              <div className='lg:w-36 lg:h-36  w-20 h-20   ml-4 lg:mt-5  shadow-md bg-gray-200 flex rounded-sm items-center justify-center  transition-all duration-300 border border-gray-200'>
+              <div
+               
+                className='lg:w-36 lg:h-36  w-20 h-20   ml-4 lg:mt-5  shadow-md bg-gray-200 flex rounded-sm items-center justify-center  transition-all duration-300 border border-gray-200'
+              >
                 {user.profilePic ? (
                   <div className='relative lg:w-36 lg:h-36 w-20 h-20 rounded-sm '>
                     <img
@@ -217,7 +238,7 @@ const PersonalDetails = () => {
                             type='file'
                             className='hidden'
                             id='changeProfilePic'
-                            onChange={handleFileSelect}
+                            onChange={e => handleFileSelect(e, 'profilepic')}
                           />
                         </label>
                       </button>
@@ -250,14 +271,14 @@ const PersonalDetails = () => {
                           className='hidden'
                           name='profileUpload'
                           id='profileUpload'
-                          onChange={handleFileSelect}
+                          onChange={e => handleFileSelect(e, 'profilepic')}
                         />
                       </label>
                     </div>
                   </div>
                 )}
               </div>
-              <div>
+              <div className='m-2'>
                 <label for='coverUpload'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -271,14 +292,8 @@ const PersonalDetails = () => {
                     type='file'
                     className='hidden'
                     id='coverUpload'
-                    onChange={e => setProfileCover(e.target.files[0])}
+                    onChange={e => handleFileSelect(e, 'coverpic')}
                   />
-                  <button
-                    className='bg-white'
-                    onClick={handleProfileCoverUpload}
-                  >
-                    Upload
-                  </button>
                 </label>
               </div>
             </div>
@@ -427,7 +442,7 @@ const PersonalDetails = () => {
               onClick={e => e.stopPropagation()}
             >
               <h2 className='text-lg font-semibold mb-2 w-full flex justify-center'>
-                File Preview
+                 Preview
               </h2>
               {previewURL && (
                 <img
@@ -468,6 +483,65 @@ const PersonalDetails = () => {
         )}
       </AnimatePresence>
 
+      <AnimatePresence>
+        {previewCoverModalOpen && (
+          <motion.div
+            className='fixed inset-0 bg-black bg-opacity-50 z-50 min-w-60 flex items-center justify-center'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            onClick={() => setPreviewModalOpen(false)}
+          >
+            <motion.div
+              className='bg-white rounded-sm p-6 w-full max-w-md shadow-lg'
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              onClick={e => e.stopPropagation()}
+            >
+              <h2 className='text-lg font-semibold mb-2 w-full flex justify-center'>
+                 Preview
+              </h2>
+              {previewURL && (
+                <img
+                  src={previewURL}
+                  alt='Selected File Preview'
+                  className=' mb-4 min-w-65'
+                />
+              )}
+              <button
+                onClick={handleProfileCoverUpload}
+                className='px-4  py-2 bg-violet-950 text-white rounded hover:bg-violet-900 w-full flex justify-center transition-all duration-300'
+              >
+                {loading ? (
+                  <span className='animate-spin'>
+                    <svg
+                      aria-hidden='true'
+                      class='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+                      viewBox='0 0 100 101'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                        fill='currentColor'
+                      />
+                      <path
+                        d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                        fill='currentFill'
+                      />
+                    </svg>
+                  </span>
+                ) : (
+                  'Submit'
+                )}
+              </button>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       {/* Model for edit */}
 
       <Modal
